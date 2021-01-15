c2850f1f8cc77af62f863c1fe8e8781e0ddbdbb0
Fabian Finalé Franqui <fabianfinale@gmail.com>
2021-01-15 14:20:36 -0500

    Testing search box

--- gvsigol/gvsigol_core/static/js/lib/viewer.js
+++ gvsigol/gvsigol_core/static/js/lib/viewer.js
@@ -130,26 +130,26 @@ viewer.core = {
     	});
       
   
-      var geocoder = new Geocoder('nominatim', {
-        // provider: 'mapquest',
-        // key: '__some_key__',
-        autoComplete: true,
-        lang: 'en-US', //en-US, fr-FR
-        placeholder: 'Search for ...',
-        targetType: 'text-input',
-        limit: 10,
-        keepOpen: true
-      });
-      map.addControl(geocoder);
+    var geocoder = new Geocoder('nominatim', {
+      // provider: 'mapquest',
+      // key: '__some_key__',
+      autoComplete: true,
+      lang: 'en-US', //en-US, fr-FR
+      placeholder: 'Search for ...',
+      targetType: 'text-input',
+      limit: 10,
+      keepOpen: true
+    });
+    
 
-      geocoder.on('addresschosen', function(evt){
-        var feature = evt.feature,
-        coord = evt.coordinate,
-        address = evt.address;
-        // some popup solution
-        // content.innerHTML = '<p>'+ address.formatted +'</p>';
-        // overlay.setPosition(coord);
-      });
+    geocoder.on('addresschosen', function(evt){
+      var feature = evt.feature,
+      coord = evt.coordinate,
+      address = evt.address;
+      // some popup solution
+      // content.innerHTML = '<p>'+ address.formatted +'</p>';
+      // overlay.setPosition(coord);
+    });
 
 		var interactions = ol.interaction.defaults({altShiftDragRotate:false, pinchRotate:false});
 		this.overviewmap = new ol.control.OverviewMap({
@@ -200,6 +200,8 @@ viewer.core = {
 			source: new ol.source.Vector()
 		});
 		this.map.addLayer(this.extentLayer);
+    
+    this.map.addControl(geocoder);
 
 		var projectionSelect = document.getElementById('custom-mouse-position-projection');
 	    projectionSelect.addEventListener('change', function(event) {
